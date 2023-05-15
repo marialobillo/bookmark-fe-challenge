331caac1d23d8234833d609f7a3305db91321194
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2023-05-15 20:25:17 +0200

    feat: correct content for the tabs

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
 const tabsArray = Array.from(document.querySelectorAll(".tab"))
 const contentArray = Array.from(document.querySelectorAll('.content'))
 
-
+// tabs on features section
 tabsArray.forEach(tab => {
     tab.addEventListener("click", () => {
         target = tab
-        console.log(tabsArray[0])
         tabsArray.forEach(tab => {
             tab.classList.remove("border-b-4")
             tab.classList.remove("border-red-500")
         })
 
         const currentTab = tabsArray.indexOf(target)
-    
         contentArray.forEach(content =>{
             if(contentArray.indexOf(content) === currentTab){
                 content.classList.remove("hidden")
@@ -26,7 +24,7 @@ tabsArray.forEach(tab => {
     })
 })
 
-
+// Faq menu accordion
 const faqs = document.querySelectorAll('.faq');
 
 faqs.forEach(faq => {
